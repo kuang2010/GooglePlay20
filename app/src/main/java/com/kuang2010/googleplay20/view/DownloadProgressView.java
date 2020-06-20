43bda589b7f87454cbef37b60cbfcdb4b3123732
kuangzeyu2019 <gdf>
2020-06-20 21:19:32 +0800

    自定义view父控件

--- app/src/main/java/com/kuang2010/googleplay20/view/DownloadProgressView.java
+++ app/src/main/java/com/kuang2010/googleplay20/view/DownloadProgressView.java
@@ -43,7 +43,9 @@ public class DownloadProgressView extends RelativeLayout {
     }
 
     private void init() {
-        mView = LayoutInflater.from(getContext()).inflate(R.layout.view_download_progress, null);
+        //以下两种方式添加mView,会使mView的父控件不是同一个
+//        mView = LayoutInflater.from(getContext()).inflate(R.layout.view_download_progress, this);//方式一添加会使得该自定义view<==>mview即自定义view=mView成为同一个控件且属性叠加，mView的父控件即自定义view的父控件。
+        mView = LayoutInflater.from(getContext()).inflate(R.layout.view_download_progress, null);//方式二添加会使mView作为自定义view的子控件添加进自定义view中，自定义view!=mView，mView的宽高、margin属性会被设置无效，变为wrap_content，mView的父控件则是该自定义view。
         RelativeLayout.LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         layoutParams.addRule(CENTER_IN_PARENT);
         addView(mView,layoutParams);
@@ -72,10 +74,15 @@ public class DownloadProgressView extends RelativeLayout {
     }
 
 
-    /**getLeft()时控件相对于父控件的左边距位置**/
+    /**注意view.getLeft()是控件view 相对 于view的 父 控件的左边距，要找准mView的父控件**/
     @Override
     protected void dispatchDraw(Canvas canvas) {
         super.dispatchDraw(canvas);// (图标,和文字)
+        int left = this.getLeft();//自定义view的相对（自定义view的父控件，很可能是屏幕）位置
+        Log.d("dispatchDraw","left:"+left);
+        int left1 = mView.getLeft();//内根mView相对于 自定义view 或 自定义view的父控件 的位置，当以方式一添加时内根view就是自定义view，此时的父控件是自定义view的父控件。
+        Log.d("dispatchDraw","left1:"+left1);
+        //在自定义中RectF等区域里的left... 位置 也是个相对值，是相对于该自定义控件的位置， 坐标原点在自定义控件的左上角。
         //在mIv_download_progress的区域里画弧：
         RectF oval = new RectF(mView.getLeft()+mIv_download_progress.getLeft(),mView.getTop()+mIv_download_progress.getTop(),mView.getLeft()+mIv_download_progress.getLeft()+mIv_download_progress.getWidth(),mView.getTop()+mIv_download_progress.getTop()+mIv_download_progress.getHeight());
         float startAngle = -90;//起始弧度

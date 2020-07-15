a4fc2557a0a23f0bf64e48a2cb3cc878c3232d2d
kuangzeyu2019 <gdf>
2020-07-15 14:07:46 +0800

    将流数据转成字符数据

--- app/src/main/java/com/kuang2010/googleplay20/util/IOUtils.java
+++ app/src/main/java/com/kuang2010/googleplay20/util/IOUtils.java
@@ -1,7 +1,9 @@
 package com.kuang2010.googleplay20.util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class IOUtils {
 	/** 关闭流 */
@@ -14,5 +16,43 @@ public class IOUtils {
 			}
 		}
 		return true;
+	}
+
+
+
+	/**
+	 * 将流数据转成字符数据
+	 * @param in
+	 * @return
+	 */
+	public static String convert2String(InputStream in) throws IOException {
+		//底层流对拷，或BufferedReader都可以
+
+        /*BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        String line = reader.readLine();
+        StringBuilder builder = new StringBuilder();
+        builder.append(line);
+        while (!TextUtils.isEmpty(line)){
+            String line1 = reader.readLine();
+            builder.append(line1);
+
+        }
+        String result = builder.toString();*/
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		byte[] buf = new byte[1024];
+		int len;
+		while ((len=in.read(buf))>0){
+			out.write(buf,0,len);
+		}
+
+		if (out.toString().toLowerCase().contains("utf-8")){
+			return out.toString("utf-8");
+		}else if (out.toString().toLowerCase().contains("iso8859-1")){
+			return out.toString("iso8859-1");
+		}else if (out.toString().toLowerCase().contains("gbk")){
+			return out.toString("gbk");
+		}
+		return out.toString();
 	}
 }
